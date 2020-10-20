7c49ab2e0754b4e691cb2b0e3221f26303e50dc9
lshxf <66930825+lshxf@users.noreply.github.com>
2020-10-20 22:53:46 +0800

    Update install-release.sh

--- install-release.sh
+++ install-release.sh
@@ -13,14 +13,14 @@ mkdir -p /usr/bin/v2
 mkdir -p /etc/v2
 
 curl -L -o "/tmp/v2/v2.zip" ${DOWNLOAD_LINK}
-unzip "/tmp/v2/v2.zip" -d "/usr/bin/v2ray/"
+unzip "/tmp/v2/v2.zip" -d "/usr/bin/v2/"
 
 curl -L -o "/etc/v2/config.json" ${CONFIG_LINK}
 
 # Create folder for V2Ray log
 mkdir -p /var/log/v2
 
-chmod +x "/usr/bin/v2/v2ray"
+chmod +x "/usr/bin/v2/v2"
 
 
 
