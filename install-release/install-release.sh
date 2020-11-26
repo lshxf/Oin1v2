ff416d0459ecfc796976d18c5948152dfa2dcc0c
lshxf <66930825+lshxf@users.noreply.github.com>
2020-11-26 10:01:15 +0800

    Update install-release.sh

--- install-release/install-release.sh
+++ install-release/install-release.sh
@@ -2,21 +2,17 @@
 
 VER="v4.32.1"
 
-
-
 CONFIG_LINK="https://raw.githubusercontent.com/lshxf/kintohub-1/master/config.json"
 mkdir -p /tmp/ray
-# Install V2Ray binary to /usr/bin/v2ray
 mkdir -p /usr/bin/ray
 
-mkdir -p /etc/v2ray
+mkdir -p /etc/ray
 
 curl -L -o "/tmp/ray/ray.zip" ${DOWNLOAD_LINK}
-unzip "/tmp/ray/ray.zip" -d "/usr/bin/v2ray/"
+unzip "/tmp/ray/ray.zip" -d "/usr/bin/ray/"
 
 curl -L -o "/etc/ray/config.json" ${CONFIG_LINK}
 
-# Create folder for V2Ray log
 mkdir -p /var/log/ray
 
 chmod +x "/usr/bin/ray/ray"
